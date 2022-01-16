75b35f890bf25c82419014e327a70282ed06fc9a
Parabellum12 <86533603+Parabellum12@users.noreply.github.com>
2022-01-16 15:08:36 -0800

    weapon scriptable objects part 1

---
+++ Assets/CharacterData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : MonoBehaviour
+{
+    [SerializeField] WeaponScriptableObject Primary_Weapon;
+    [SerializeField] WeaponScriptableObject Secondary_Weapon;
+}
