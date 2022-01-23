4017331f32472bc2ae742a33c8962f01d66d17b0
Parabellum12 <86533603+Parabellum12@users.noreply.github.com>
2022-01-22 22:27:23 -0800

    started adding level editor functionality

---
+++ Assets/SaveLoad_Handler_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaveLoad_Handler_Script : MonoBehaviour
+{
+    [SerializeField] TMP_Dropdown mapSize;
+    [SerializeField] TMP_Text mapName;
+    [SerializeField] TMP_Dropdown existingFileName;
+    [SerializeField] TMP_Text saveAsName;
+
+
+
+
+
+    
+}
---
+++ Assets/World_Handler_Script.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World_Handler_Script : MonoBehaviour
+{
+    GridClass<BulletFireLine> bulletPath;
+    List<GridClass<WorldBuildTile>> buildLevels;
+
+    public void GenerateNewMap()
+    {
+
+    }
+
+
+
+
+
+    public class WorldBuildTile
+    {
+
+        public WorldBuildTile()
+        {
+
+        }
+    }
+
+    public class BulletFireLine
+    {
+
+        public BulletFireLine()
+        {
+
+        }
+    }
+}
